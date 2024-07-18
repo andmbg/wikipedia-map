32e1a72fa96720159099009ff65d3821852f3f8d
Andreas Domberg <andom@posteo.de>
2024-07-18 08:46:09 +0200

    add metadata & preview

---
+++ __init__.py
@@ -0,0 +1,6 @@
+metadata = {
+    "title": "Wikimap: Geografisch verankertes Wissen",
+    "route": "/wikimap/",
+    "thumbnail": "dashapps/wikimap/preview.png",
+    "synopsis": "Wikipedia-Artikel sind oft mit einer Position auf dem Globus verbunden. Diese Karte erlaubt es, auf der Weltkarte entlang zu stöbern und zu sehen, welche Artikel wo verortet sind und wie oft sie aufgerufen werden. Ein Einführungstext gibt einen ersten Einblick und verlinkt den Artikel.",
+}
