ccfdf47f1ddde91cbfc8b1a071675af90a3397ac
Andreas Domberg <andom@posteo.de>
2024-08-10 12:15:15 +0200

    lang: metadata

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 metadata = {
-    "title": "Wikimap: Geografisch verankertes Wissen",
-    "route": "/wikimap/",
-    "thumbnail": "dashapps/wikimap/preview.png",
-    "synopsis": "Wikipedia-Artikel sind oft mit einer Position auf dem Globus verbunden. Diese Karte erlaubt es, auf der Weltkarte entlang zu stöbern und zu sehen, welche Artikel wo verortet sind und wie oft sie aufgerufen werden. Ein Einführungstext gibt einen ersten Einblick und verlinkt den Artikel.",
+    "title": "Wikimap: Geographically anchored knowledge",
+    "route": "/en/wikimap/",
+    "thumbnail": "dashapps/en/wikimap/preview.png",
+    "synopsis": "Many Wikipedia articles are geographically anchored. This map lets you browse across the globe and see where, and how popular they are. By clicking on them, you can read a preview, so you can e.g. explore a holiday location or your neighborhood.",
 }
