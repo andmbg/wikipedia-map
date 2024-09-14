e64c369f0b7a359680af46f6c92542e1c6613270
Andreas Domberg <andom@posteo.de>
2024-09-14 15:00:10 +0200

    fix mixed-up languages in metadata

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 metadata = {
     "de": {
-        "title": "Wikimap: Geographically anchored knowledge",
+        "title": "Wikimap: Geografisch verankertes Wissen",
         "route": "/en/wikimap/",
         "thumbnail": "dashapps/en/wikimap/preview.png",
-        "synopsis": "Many Wikipedia articles are geographically anchored. This map lets you browse across the globe and see where, and how popular they are. By clicking on them, you can read a preview, so you can e.g. explore a holiday location or your neighborhood.",
+        "synopsis": "Wikipedia-Artikel sind oft mit einer Position auf dem Globus verbunden. Diese Karte erlaubt es, auf der Weltkarte entlang zu stöbern und zu sehen, welche Artikel wo verortet sind und wie oft sie aufgerufen werden. Ein Einführungstext gibt einen ersten Einblick und verlinkt den Artikel.",
     },
     "en": {
-        "title": "Wikimap: Geografisch verankertes Wissen",
+        "title": "Wikimap: Geographically anchored knowledge",
         "route": "/de/wikimap/",
         "thumbnail": "dashapps/de/wikimap/preview.png",
-        "synopsis": "Wikipedia-Artikel sind oft mit einer Position auf dem Globus verbunden. Diese Karte erlaubt es, auf der Weltkarte entlang zu stöbern und zu sehen, welche Artikel wo verortet sind und wie oft sie aufgerufen werden. Ein Einführungstext gibt einen ersten Einblick und verlinkt den Artikel.",
+        "synopsis": "Many Wikipedia articles are geographically anchored. This map lets you browse across the globe and see where, and how popular they are. By clicking on them, you can read a preview, so you can e.g. explore a holiday location or your neighborhood.",
     }
 }
